7d13d06cc3e93d0028562639be0b265359079bb8
Amazedon <amazedon50@gmail.com>
2026-07-31 00:26:50 +0100

    Fix icon file paths in service worker
    Corrected the path for icon files in the CORE_ASSETS array.

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CACHE_NAME = "zed-cache-v1";
 const CORE_ASSETS = [
   "/app.html",
   "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
